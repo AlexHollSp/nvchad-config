0a6f5789b5a4605f59b967b56536a64811276bf5
alexhollsp <alexhollsp@outlook.com>
2025-02-15 11:41:43 -0300

    Add plugin LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,6 +16,34 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.harper_ls.setup {
+  settings = {
+    ["harper-ls"] = {
+      linters = {
+        spell_check = true,
+        spelled_numbers = false,
+        an_a = true,
+        sentence_capitalization = true,
+        unclosed_quotes = true,
+        wrong_quotes = false,
+        long_sentences = true,
+        repeated_words = true,
+        spaces = true,
+        matcher = true,
+        correct_number_suffix = true,
+        number_suffix_capitalization = true,
+        multiple_sequential_pronouns = true,
+        linking_verbs = false,
+        avoid_curses = true,
+        terminating_conjunctions = true
+      }
+    }
+  },
+}
+
+
+
+
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
